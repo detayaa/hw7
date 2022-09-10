d7eda30d7c6ac6f689323d7b8bf2de61c9f541a4
detayaa <drugochenko.taya@mail.ru>
2022-09-10 23:59:59 +0700

    1 2

---
+++ src/main.java
@@ -0,0 +1,21 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        int month = 0;
+        int money = 0;
+        while (money < 2_459_000) {
+            month += 1;
+            money += 1;
+        } System.out.println("Месяц " + month + ", сумма накоплений равна " + money + " рублей");
+
+
+
+        System.out.println("Задание 2");
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.println(i);
+        }
+
+    }
+}
