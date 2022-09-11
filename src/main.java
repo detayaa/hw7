76e528bb20da3c24d77dd0556ad3a024a5a47f51
detayaa <drugochenko.taya@mail.ru>
2022-09-11 14:06:11 +0700

    3

--- src/main.java
+++ src/main.java
@@ -5,7 +5,7 @@ public class main {
         int money = 0;
         while (money < 2_459_000) {
             month += 1;
-            money += 1;
+            money += 60000;
         } System.out.println("Месяц " + month + ", сумма накоплений равна " + money + " рублей");
 
 
@@ -14,8 +14,25 @@ public class main {
         int i = 0;
         while (i < 10) {
             i++;
-            System.out.println(i);
+            System.out.print(i + " ");
         }
+
+
+        for (int a = 10; a >= 1; a --) {
+            System.out.print(a + " ");
+        }
+
+        System.out.println("Задание 3");
+        int people = 12000000;
+        int born = 17 * (people / 1000);
+        int die = 8 * (people / 1000);
+        int allPeople = people + (born - die);
+        for (int year = 1; year <= 10; year ++ ){
+            allPeople = people + allPeople;
+            System.out.println("Год " + year + " численность населения составляет " + allPeople);
+        }
+
+
 
     }
 }
