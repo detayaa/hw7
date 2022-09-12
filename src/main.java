9136239429af521351382f1155b5ac243cc5bacd
detayaa <drugochenko.taya@mail.ru>
2022-09-12 15:25:15 +0700

    1.3

--- src/main.java
+++ src/main.java
@@ -6,8 +6,8 @@ public class main {
         while (money < 2_459_000) {
             month += 1;
             money += 60000;
-        } System.out.println("Месяц " + month + ", сумма накоплений равна " + money + " рублей");
-
+        }
+        System.out.println("Месяц " + month + ", сумма накоплений равна " + money + " рублей");
 
 
         System.out.println("Задание 2");
@@ -18,7 +18,7 @@ public class main {
         }
 
 
-        for (int a = 10; a >= 1; a --) {
+        for (int a = 10; a >= 1; a--) {
             System.out.print(a + " ");
         }
 
@@ -27,17 +27,27 @@ public class main {
         int born = 17 * (people / 1000);
         int die = 8 * (people / 1000);
         int allPeople = people + (born - die);
-        for (int year = 1; year <= 10; year ++ ){
+        for (int year = 1; year <= 10; year++) {
             allPeople = people + allPeople;
             System.out.println("Год " + year + " численность населения составляет " + allPeople);
         }
 
         System.out.println("Задание 1.3");
         int salary = 15000;
-        for (int r = 1; salary <= 12_000_000; r ++) {
+        for (int r = 1; salary <= 12_000_000; r++) {
             salary += ((salary * 7) / 100) + salary;
             if (r % 6 == 0) {
                 System.out.println("Месяц " + r + " сумма равна " + salary);
+            }
+        }
+        System.out.println("Задание 3.3");
+        int salaryy = 15000;
+        int m = 1;
+        while (m < 108) {
+            m ++;
+            salaryy += ((salaryy * 7) / 100) + salaryy;
+            if (m % 6 == 0) {
+                System.out.println("Месяц " + m + " сумма равна " + salaryy);
             }
         }
     }
