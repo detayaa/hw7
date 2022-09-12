0d7ecabfd57043c49af10d03d4d135269e515e89
detayaa <drugochenko.taya@mail.ru>
2022-09-12 21:59:50 +0700

    1.3

--- src/main.java
+++ src/main.java
@@ -32,7 +32,7 @@ public class main {
             System.out.println("Год " + year + " численность населения составляет " + allPeople);
         }
 
-        System.out.println("Задание 1.3");
+        System.out.println("Задание 1.2");
         int salary = 15000;
         for (int r = 1; salary <= 12_000_000; r++) {
             salary += ((salary * 7) / 100) + salary;
@@ -40,7 +40,7 @@ public class main {
                 System.out.println("Месяц " + r + " сумма равна " + salary);
             }
         }
-        System.out.println("Задание 3.3");
+        System.out.println("Задание 3.2");
         int salaryy = 15000;
         int m = 1;
         while (m < 108) {
@@ -50,7 +50,7 @@ public class main {
                 System.out.println("Месяц " + m + " сумма равна " + salaryy);
             }
         }
-        System.out.println("Задание 4.3");
+        System.out.println("Задание 4.2");
         int friday = 7;
         System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет.");
         while (friday < 31){
@@ -59,5 +59,24 @@ public class main {
                 System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет.");
             }
         }
+        System.out.println("Задание 1.3");
+        int before = 2022 - 200;
+        int future = 2022 + 100;
+        while (before < 2022) {
+            before++;
+            if (before % 79 == 0) {
+                System.out.println(before);
+            }
+        }
+        while (future > 2022) {
+            future --;
+            if (future % 79 == 0) {
+                System.out.println(future);
+            }
+        }
+
+
+
+
     }
 }
